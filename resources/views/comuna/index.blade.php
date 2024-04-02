b9306c739962c1244aa4c5332e80102910734597
Alison Angulo <alisonangulodiosa@gmail.com>
2024-04-01 20:49:02 -0500

    Comuna List

---
+++ resources/views/comuna/index.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de Municipios</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Listado de Municipios</h1>
+      <a href="" class="btn btn-success">Add</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Commune</th>
+                    <th scope="col">Municipality</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+             @foreach ($comunas as $comuna)
+                <tr>
+                    <th scope="row">{{ $comuna->comu_codi }}</th>
+                    <td>{{ $comuna->comu_nomb }}</td>
+                    <td>{{ $comuna->muni_codi }}</td>
+                    <td>Actions</td>
+                </tr>
+             @endforeach
+            </tbody>
+        </table>
+    </div>
+    
+
+    
+   </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComunaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [ComunaController:: class, 'index'])->name('comunas.index');

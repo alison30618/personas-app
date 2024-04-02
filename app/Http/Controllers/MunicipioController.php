cc0e2acdcde2b73b19427094b73a3cdf44652daa
Alison Angulo <alisonangulodiosa@gmail.com>
2024-04-01 21:42:21 -0500

    Municipios Create

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -29,18 +29,27 @@ class MunicipioController extends Controller
      */
     public function create()
     {
-        //
+        $departamentos = DB::table('tb_departamento')
+        ->orderBy('depa_nomb')
+        ->get();
+        return view('municipio.new', ['departamentos' => $departamentos]);
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $municipio = new Municipio();
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*','tb_departamento.depa_nomb')
+        ->get();
+        return view('municipio.index', ['municipios' => $municipios]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class , 'edit'])->name('comunas.edit');
 Route::get('/municipios', [MunicipioController:: class, 'index'])->name('municipios.index');
+Route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
+Route::get('/municipios/create', [MunicipioController::class , 'create'])->name('municipios.create');
 
 
   
